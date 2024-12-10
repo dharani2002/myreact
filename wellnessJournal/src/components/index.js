fcf9a945c5454ee12578065cfe98d2dcf0693d34
dharani2002 <rdharani25@gmail.com>
2024-12-10 13:46:42 +0530

    wellness journal complete

--- wellnessJournal/src/components/index.js
+++ wellnessJournal/src/components/index.js
@@ -2,10 +2,11 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Home from "./Home";
 import MoodTracker from "./MoodTracker";
-
+import Journal from "./Journal";
 
 
 export {Header}
 export {Footer}
 export {Home}
 export {MoodTracker}
+export {Journal}
