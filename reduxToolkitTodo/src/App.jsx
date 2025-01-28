7ce2d62aae0fcf867fecefacdc1318c29effe972
dharani2002 <rdharani25@gmail.com>
2025-01-28 11:01:29 +0530

    redux crash course

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>redux crash course</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
